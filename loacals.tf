6a9b0c598084d95afce396b086a588b33ebfda9d
pedroorf1 <pedroorf@gmail.com>
2024-10-20 20:29:01 -0300

    apply subs

--- loacals.tf
+++ loacals.tf
@@ -1,7 +1,7 @@
 locals {
   common_tags = {
     Project   = "infra eks terraform"
-    CreatedAt = "20-10-2024"
+    CreatedAt = "2024-10-20"
     ManagedBy = "Terraform"
     Owner     = "Pedro Ribeiro"
     Service   = "Auto Scaling App"
