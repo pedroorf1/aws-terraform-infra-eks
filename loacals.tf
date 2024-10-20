2d5cddbb0aa1eefc66d58fdedd8788fd21fc2c53
pedroorf1 <pedroorf@gmail.com>
2024-10-20 18:55:41 -0300

    first

---
+++ loacals.tf
@@ -0,0 +1,9 @@
+locals {
+  common_tags={
+    Project = "infra eks terraform"
+    CreatedAt = "20-10-2024"
+    ManagedBy = "Terraform"
+    Owner = "Pedro Ribeiro"
+    Service = "Auto Scaling App"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "profile" {
+  type = string
+  default = "aws-pessoal"
+}
+
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "service_name" {
+  type = string
+  default = "autoscaling-app"
+}
