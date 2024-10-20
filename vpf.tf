0f97fa61ca571d90706083c5bc419c7ce2d752cb
pedroorf1 <pedroorf@gmail.com>
2024-10-20 20:47:31 -0300

    refactore

--- vpf.tf
+++ vpf.tf
@@ -9,30 +9,46 @@ resource "aws_internet_gateway" "this" {
   tags   = merge(local.common_tags, { Name = "Terraform GATWAY" })
 }
 
-resource "aws_subnet" "public_a" {
+resource "aws_subnet" "this" {
+
+  for_each = {
+    "pub_a" : ["192.168.1.0/24", "${var.region}a", "Terraform SUB_PUBLIC_a"]
+    "pub_b" : ["192.168.2.0/24", "${var.region}b", "Terraform SUB_PUBLIC_b"]
+    "pvt_a" : ["192.168.3.0/24", "${var.region}a", "Terraform SUB_PRIVATE_a"]
+    "pvt_b" : ["192.168.4.0/24", "${var.region}b", "Terraform SUB_PRIVATE_b"]
+  }
+
   vpc_id            = aws_vpc.this.id
-  cidr_block        = "192.168.0.0/24"
-  availability_zone = "${var.region}a"
-  tags              = merge(local.common_tags, { Name = "Terraform SUB_PUBLIC_a" })
+  cidr_block        = each.value[0]
+  availability_zone = each.value[1]
+  tags              = merge(local.common_tags, { Name = each.value[2] })
 }
 
-resource "aws_subnet" "public_b" {
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = "192.168.1.0/24"
-  availability_zone = "${var.region}b"
-  tags              = merge(local.common_tags, { Name = "Terraform SUB_PUBLIC_b" })
-}
 
-resource "aws_subnet" "private_a" {
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = "192.168.2.0/24"
-  availability_zone = "${var.region}a"
-  tags              = merge(local.common_tags, { Name = "Terraform SUB_PRIVATE_a" })
-}
+# resource "aws_subnet" "public_a" {
+#   vpc_id            = aws_vpc.this.id
+#   cidr_block        = "192.168.0.0/24"
+#   availability_zone = "${var.region}a"
+#   tags              = merge(local.common_tags, { Name = "Terraform SUB_PUBLIC_a" })
+# }
 
-resource "aws_subnet" "private_b" {
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = "192.168.3.0/24"
-  availability_zone = "${var.region}b"
-  tags              = merge(local.common_tags, { Name = "Terraform SUB_PRIVATE_b" })
-}
+# resource "aws_subnet" "public_b" {
+#   vpc_id            = aws_vpc.this.id
+#   cidr_block        = "192.168.1.0/24"
+#   availability_zone = "${var.region}b"
+#   tags              = merge(local.common_tags, { Name = "Terraform SUB_PUBLIC_b" })
+# }
+
+# resource "aws_subnet" "private_a" {
+#   vpc_id            = aws_vpc.this.id
+#   cidr_block        = "192.168.2.0/24"
+#   availability_zone = "${var.region}a"
+#   tags              = merge(local.common_tags, { Name = "Terraform SUB_PRIVATE_a" })
+# }
+
+# resource "aws_subnet" "private_b" {
+#   vpc_id            = aws_vpc.this.id
+#   cidr_block        = "192.168.3.0/24"
+#   availability_zone = "${var.region}b"
+#   tags              = merge(local.common_tags, { Name = "Terraform SUB_PRIVATE_b" })
+# }
